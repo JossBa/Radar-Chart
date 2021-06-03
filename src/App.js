e7ee0f428ddd70a9b1e3008bed2d8c1fd22a2067
Luis Hankel <kaes3kuch3n@gmail.com>
2021-06-03 14:28:45 +0200

    Moved chart config and data to component props for easier use

--- src/App.js
+++ src/App.js
@@ -4,9 +4,29 @@ import CarDetail from "./components/CarDetail";
 import RadarChart from "./components/RadarChart";
 
 class App extends Component {
-    constructor() {
-        super();
-        this.state = {cars: []};
+    constructor(props) {
+        super(props);
+        this.state = {
+            cars: [],
+            chartConfig: {
+                axes: {
+                    maxValue: 10
+                }
+            },
+            chartData: {
+                labels: ["Value 1", "Value 2", "Value 3", "Value 4", "Value 5", "Value 6"],
+                datasets: [
+                    {
+                        data: [6, 5, 8, 4, 3, 7],
+                        color: "#ff0000"
+                    },
+                    {
+                        data: [4, 7, 5, 6, 9, 6],
+                        color: "#0000ff"
+                    }
+                ]
+            }
+        };
     }
 
     async componentDidMount() {
@@ -18,7 +38,7 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <RadarChart />
+                <RadarChart width={500} height={500} config={this.state.chartConfig} data={this.state.chartData} />
                 <header className="App-header">
                     <p> Hellooooo { this.state.cars.map(c => c.car).join(",\n") || ""} </p>
                     <CarDetail carName="VW Käfer" />
--- src/components/RadarChart.jsx
+++ src/components/RadarChart.jsx
@@ -6,24 +6,8 @@ export default class RadarChart extends Component {
         super(props);
 
         this.state = {
-            chartConfig: {
-                axes: {
-                    maxValue: 10
-                }
-            },
-            chartData: {
-                labels: ["Value 1", "Value 2", "Value 3", "Value 4", "Value 5", "Value 6"],
-                datasets: [
-                    {
-                        data: [6, 5, 8, 4, 3, 7],
-                        color: "#ff0000"
-                    },
-                    {
-                        data: [4, 7, 5, 6, 9, 6],
-                        color: "#0000ff"
-                    }
-                ]
-            }
+            config: props.config,
+            data: props.data
         };
     }
 
@@ -31,13 +15,13 @@ export default class RadarChart extends Component {
         const canvas = document.querySelector(".radar-chart > canvas");
         const ctx = canvas.getContext("2d");
 
-        new Chart(ctx, this.state.chartData, this.state.chartConfig).draw();
+        new Chart(ctx, this.state.data, this.state.config).draw();
     }
 
     render() {
         return (
             <div className="radar-chart">
-                <canvas width="1000" height="1000" />
+                <canvas width={this.props.width} height={this.props.height} />
             </div>
         );
     }
